cd9f9cd964eb24c7a84d2280460a48fc8fb3e897
= <=>
2022-03-06 20:04:12 +0700

    praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,36 @@ use App\Http\Controllers\ArticleController;
 // Route::get('/about', [PageController::class, 'about']);
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
 //================================================================================================
-//praktikum 2.3
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'about']);
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+// praktikum 2.3
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/about', [AboutController::class, 'about']);
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+//================================================================================================
+//praktikum 3
+// Halaman tentang Home
+Route::get('/', function () { return 'Home Page!'; });
+
+// Halaman tentang Products
+Route::prefix('/category')->group(function () {
+    Route::get('/marbel-edu-games', function () { return 'marbel-edu-games';              });
+    Route::get('/marbel-and-friends-kids-games', function () { return 'marbel-and-friends-kids-games'; });
+    Route::get('/riri-story-books', function () { return 'riri-story-books';              });
+    Route::get('/kolak-kids-songs', function () { return 'kolak-kids-songs';              });
+});
+
+// Halaman tentang News
+Route::get('/news'       , function (){ return 'news page';      });
+Route::get('/news/{slug}', function ($NewsTopic){ return 'News Topic: ' . $NewsTopic; });
+
+// Halaman tentang Program
+Route::prefix('/program')->group(function () {
+    Route::get('/karir' , function () { return 'karir';              });
+    Route::get('/magang' , function () { return 'magang';             });
+    Route::get('/kunjungan-industri', function () { return 'kunjungan-industri'; });
+});
+
+// Halaman tentang About Us
+Route::get('/about-us', function () { return 'about-us'; });
+
+// Halaman tentang Contact Us
+Route::resource('contact-us', function () { return 'resources Contact Us'; });
